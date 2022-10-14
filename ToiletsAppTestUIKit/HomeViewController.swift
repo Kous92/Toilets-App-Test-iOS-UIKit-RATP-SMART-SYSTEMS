86fe81044ec07ebc9de445f317594f7686b523cb
Koussaïla BEN MAMAR <kous92@gmail.com>
2022-10-14 11:36:46 +0200

    Ajout support Live Preview SwiftUI

--- ToiletsAppTestUIKit/HomeViewController.swift
+++ ToiletsAppTestUIKit/HomeViewController.swift
@@ -11,7 +11,42 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         view.backgroundColor = .blue
     }
 }
+
+#if canImport(SwiftUI) && DEBUG
+import SwiftUI
+
+let deviceNames: [String] = [
+    "iPhone SE (3rd generation)",
+    "iPhone 14 Pro",
+    "iPad Pro (9.7-inch)"
+]
+
+@available(iOS 13.0, *)
+struct ViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        // Prévisualisation avec plusieurs simulateurs iPhone et iPad en light et dark mode
+        ForEach(deviceNames, id: \.self) { deviceName in
+            // Mode lumineux (light mode)
+            UIViewControllerPreview {
+                HomeViewController()
+            }
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+            .preferredColorScheme(.light)
+            .previewDisplayName("\(deviceName) (Light)")
+            .edgesIgnoringSafeArea(.all)
+            
+            // Mode sombre (dark mode)
+            UIViewControllerPreview {
+                HomeViewController()
+            }
+            .previewDevice(PreviewDevice(rawValue: deviceName))
+            .preferredColorScheme(.dark)
+            .previewDisplayName("\(deviceName) (Dark)")
+            .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+#endif
